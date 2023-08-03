846300eb680f6f37fd0f5d2d48e807c1e67ba751
FamenOG <famenoranaivoarison@gmail.com>
2023-08-03 22:44:48 +0200

    delete request folder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibrarianController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+Route::get('/insert', [LibrarianController::class,"insert"]);
+
